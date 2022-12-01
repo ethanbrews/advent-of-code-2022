650c0b7643515e192aa11660efd5510a533f806a
Ethan Brews <efyeb3@nottingham.ac.uk>
2022-12-01 15:29:29 +0000

    Solution 1.1

--- src/main/kotlin/days/Day01.kt
+++ src/main/kotlin/days/Day01.kt
@@ -2,7 +2,14 @@ package days
 
 class Day01 : Day(1) {
     override fun part1() : Any {
-        return 0
+        val elves = mutableListOf(0)
+        inputList.forEach {
+            if (it == "")
+                elves.add(0)
+            else
+                elves[elves.lastIndex] += it.toInt()
+        }
+        return elves.max()
     }
 
     override fun part2() : Any {
